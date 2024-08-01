3b300377128c165e99324a703dd9cd0e8c303a7b
Keval <kevalmehta885@gmail.com>
2024-08-01 16:29:01 +0530

    Created two component

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList></FriendList>
+        <FormAddFriend></FormAddFriend>
+        <Button>Add Friend</Button>
       </div>
     </div>
   );
@@ -57,7 +59,23 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend Name</label>
+      <input type="text"></input>
+      <label>🌄 Image URL</label>
+      <input type="text"></input>
+      <Button>Add</Button>
+    </form>
   );
 }
